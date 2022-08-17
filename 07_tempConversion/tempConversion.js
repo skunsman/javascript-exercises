726ef4c1fe2312f3e004bc1ba3828ed856b1c41f
Scott Kunsman <skunsman@overdrive.com>
2022-08-16 21:24:03 -0400

    fix parenthesis

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const ftoc = function(temperature) {
-  return Math.round(((temperature - 32) * (5/9)) * 10) / 10;
+  return Math.round((temperature - 32) * (5/9) * 10) / 10;
 };
 
 const ctof = function(temperature) {
-  return Math.round((temperature * (9/5) + 32) * 10) / 10;
+  return Math.round(((temperature * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
