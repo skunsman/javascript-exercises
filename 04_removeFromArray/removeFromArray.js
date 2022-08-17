f435a9907acf03535f9823709405a343e56ce960
Scott Kunsman <skunsman@overdrive.com>
2022-08-16 20:41:44 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
+    let list = arguments[0];
+    // console.log(arguments.length);
+    
+    for (let i = 1; i < arguments.length; i++) {
+        // console.log(arguments[i]);
+        const index = list.indexOf(arguments[i]);
 
+        if (index > -1)
+            list.splice(index, 1);
+    }
+    
+    console.log(list);
+    return list;
 };
 
 // Do not edit below this line
