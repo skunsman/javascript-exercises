781d9ce1d1534c2b973edaf6bd217e4b1dd27139
Scott Kunsman <skunsman@overdrive.com>
2022-08-16 20:18:35 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    const inputLetters = input.split('').reverse();
+    
+    let output = '';
+    inputLetters.forEach(letter => {
+        output += letter;
+    });
 
+    return output;
 };
 
 // Do not edit below this line
