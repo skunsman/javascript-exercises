720c9fe0a1bddd0fa20ac98e288c43274278a064
Scott Kunsman <skunsman@overdrive.com>
2022-08-16 20:52:13 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    } else if (typeof(start) != "number" || typeof(end) != "number" ) {
+        return 'ERROR';
+    }
 
+    if (start > end) {
+        const newEnd = start;
+        start = end;
+        end = newEnd;
+    }
+    let total = 0;
+
+    for (let i = start; i <= end; i++) {
+        total += i;
+    }
+    
+    return total;
 };
 
 // Do not edit below this line
