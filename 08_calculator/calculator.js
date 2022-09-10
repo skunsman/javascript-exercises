f5e5fa69c64c32022972dc9c94f9c96d9ac26599
Scott Kunsman <skunsman@overdrive.com>
2022-09-09 20:54:47 -0400

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+  return list.reduce((previous, current) => previous + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(list) {
+  let total = 1;
+  list.forEach(num => {
+    total *= num;
+  });
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+    return -1;
+  } else if (num == 0) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
