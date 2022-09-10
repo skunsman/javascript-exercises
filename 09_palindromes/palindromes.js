4df35132791cc40ec196898939ec7a4ee1af7131
Scott Kunsman <skunsman@overdrive.com>
2022-09-09 21:28:09 -0400

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const pali = text.toLowerCase().replace(/[^a-z0-9]/gi,'');
 
+    // for (let i = 0; i < Math.floor(pali.length / 2); i++){
+
+    //     console.log(pali[i] + ' | ' + pali[(pali.length - 1) - i]);
+    //     if (!(pali[i] == pali[(pali.length - 1) - i]))
+    //         return false;
+    // }
+    
+    // return true;
+
+    return (
+        pali
+          .split("")
+          .reverse()
+          .join("") == pali
+      );
 };
 
 // Do not edit below this line
