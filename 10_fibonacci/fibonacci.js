144292b490c40e6b1f962202bf9ad7db97972b6a
Scott Kunsman <skunsman@overdrive.com>
2022-09-10 22:57:58 -0400

    Complete challenge 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    if (fib < 0) return 'OOPS';
+    if (fib === 0) return 0;
 
+    let prev = 0;
+    let next = 1;
+
+    for (let i = 1; i < fib; i++){
+        const temp = next;
+        next = prev + next
+        prev = temp;
+    }
+
+    return next;
 };
 
 // Do not edit below this line
