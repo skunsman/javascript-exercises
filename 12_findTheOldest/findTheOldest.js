4b07acf7086bf553ac8c9df614a8f042752c76a4
Scott Kunsman <skunsman@overdrive.com>
2022-09-10 23:42:29 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((prev, next) => {
+        if (!prev.yearOfDeath) prev.yearOfDeath = new Date().getFullYear();
+        if (!next.yearOfDeath) next.yearOfDeath = new Date().getFullYear();
 
+        return ((prev.yearOfDeath - prev.yearOfBirth) >
+            (next.yearOfDeath - next.yearOfBirth)) 
+            ? prev : next;
+    });
 };
 
 // Do not edit below this line
